ce35b686d2df9b03f20ec429624a318ec7cdd44a
Johnathan <jocuog@gmail.com>
2022-03-25 21:57:13 -0600

    commit tree 4d941985abb4010b13b5953aa6847f98fcebaa10

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+const record = [ 
+        {year: '2015', result: 'W'},
+        {year: '2014', result: 'N/A'},
+        {year: '2013', result: 'L'},
+    ]
+
+
+function superbowlWin(arr) {
+    let win = arr.find(function (el) {
+        return el.result === 'W'}) 
+     if (win === undefined) {
+         return 
+    } else {
+        return win.year
+    }
+
+}
+
+//     const superbowlWin = (array) => {
+//         let win = array.find(function (element) {
+//             return element['result'] === 'W'})
+//         if (win === undefined) {
+
+//             return win 
+//         } else {
+//             console.log(win['year'])
+//             return win['year']
+//         } 
+        
+//     }
+// superbowlWin(record)
